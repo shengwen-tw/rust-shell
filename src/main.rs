6cdbbf2c890353490523a7f59da6ede2613d6f84
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-30 19:06:13 +0800

    add echo shell command

--- src/main.rs
+++ src/main.rs
@@ -472,26 +472,33 @@ impl<'a> Shell<'a> {
     }
 }
 
-fn shell_cmd_help(argc: Vec<&str>, argv: usize) {
-    Shell::puts("argc: ");
-
-    for arg in argc {
-        Shell::puts(format!("{} ", arg).as_ref());
-    }
-
-    Shell::puts(format!("\n\rargv: {}\n\r", argv).as_ref());
+fn shell_cmd_help(_argc: Vec<&str>, _argv: usize) {
+    Shell::puts("help\n\rclear\n\recho\n\r");
 }
 
 fn shell_cmd_clear(_argc: Vec<&str>, _argv: usize) {
     Shell::cls();
 }
 
+fn shell_cmd_echo(argc: Vec<&str>, argv: usize) {
+    for i in 1..argv {
+        if i != (argv - 1) {
+            Shell::puts(format!("{} ", argc[i]).as_ref());
+        } else {
+            Shell::puts(format!("{}\n\r", argc[i]).as_ref());
+        }
+    }
+}
+
 fn main() {
     let mut shell = Shell::new("shell > ");
     shell.add_command("help", shell_cmd_help);
     shell.add_command("clear", shell_cmd_clear);
+    shell.add_command("echo", shell_cmd_echo);
 
     shell.start();
+    Shell::puts("type 'help' for help\n\r");
+
     loop {
         let cmd = shell.listen();
         shell.parse(cmd.as_str());
